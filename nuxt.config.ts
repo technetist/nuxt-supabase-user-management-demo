1369a341ee414b029863477e599ccfde30390540
Adrien Maranville <12-adrien@users.noreply.git.big-api.com>
2023-08-09 14:23:21 -0600

    finalize project setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxtjs/supabase'],
+  css: ['@/assets/main.css'],
   devtools: { enabled: true }
 })
